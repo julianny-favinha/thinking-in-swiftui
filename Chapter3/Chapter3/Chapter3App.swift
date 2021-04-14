8baa2659f5d395d86758364df48fc3a7bbd6b4ef
Julianny Favinha Donda <julianny.favinha@gmail.com>
2021-04-14 11:36:43 -0300

    Basic Chapter 3 exercise

---
+++ Chapter3/Chapter3/Chapter3App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter3App.swift
+//  Chapter3
+//
+//  Created by Julianny Favinha Donda on 14/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Chapter3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
