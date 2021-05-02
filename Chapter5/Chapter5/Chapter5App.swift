ef7aa7944edb891b0d41e0df22d8444212fbfe2c
Julianny Favinha Donda <julianny.favinha@gmail.com>
2021-05-01 22:18:37 -0300

    Add chapter 5

---
+++ Chapter5/Chapter5/Chapter5App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter5App.swift
+//  Chapter5
+//
+//  Created by Julianny Favinha Donda on 01/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Chapter5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chapter5/Chapter5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chapter5
+//
+//  Created by Julianny Favinha Donda on 01/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
